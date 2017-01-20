e8890e86c83cdb5b2e27036227e59638f617e9ec
Melik Karapetyan <melik.karapetyan1996@gmail.com>
2017-01-20 22:43:06 +0000

    add canvas interface

---
+++ canvas.js
@@ -0,0 +1,118 @@
+/**
+ * Created by melontron on 1/20/17.
+ */
+
+
+var stars = [
+    {
+        mass: 100,
+        x:50,
+        y:50,
+        L: 100,
+        r: 17,
+        image: "./images/sun.png"
+    },
+    {
+        mass: 100,
+        x: 200,
+        y:40,
+        L: 100,
+        r: 19,
+        image: "./images/sun.png"
+    },
+    {
+        mass: 100,
+        x: 150,
+        y:160,
+        L: 100,
+        r: 30,
+        image: "./images/sun.png"
+    }
+];
+
+var ship = {
+    mass: 30,
+    x:10,
+    y: 50,
+    image: ""
+}
+
+var Controller = function (canvasId, stars, ship) {
+    var _this = this;
+    this.screen = {
+        w: 480,
+        h: 320
+    };
+    this.canvas = document.getElementById(canvasId);
+    this.canvas.width = this.screen.w;
+    this.canvas.height = this.screen.h;
+    this.context = this.canvas.getContext('2d');
+
+    this.timeStep = 20;
+
+
+    this.init = function () {
+        _this.stars = [];
+        stars.map(function (star) {
+            var sImage = new Image();
+                sImage.src = star.image;
+                star.image = sImage;
+                _this.stars.push(star);
+        });
+    
+        _this.ship = ship;
+        _this.render();
+        _this.update();
+    };
+
+    this.requestState = function () {
+
+    };
+
+    this.render = function () {
+        _this.clear();
+
+        this.context.beginPath();
+        this.context.fillStyle = "FF0000";
+        this.context.rect(0,0,this.canvas.width, this.canvas.height);
+        this.context.stroke();
+
+        this.context.beginPath();
+        this.context.fillStyle = "00FF00";
+        this.context.fillRect(_this.ship.x,_this.ship.y,20, 20);
+
+        this.stars.map(function (star) {
+            _this.context.beginPath();
+            _this.context.drawImage(star.image, star.x, star.y, 2* star.r, 2*star.r);
+            _this.context.stroke();
+        });
+
+
+    };
+
+    this.clear = function () {
+        _this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+    };
+
+    this.updateState = function (coords) {
+        _this.ship.x = coords.ship.x;
+        _this.ship.y = coords.ship.y;
+    };
+    this.update = function () {
+        this.interval = setInterval(function () {
+            //var coords = _this.requestState(_this.ship, _this.stars);
+            var coords = {};
+            coords.ship = {
+                x: _this.ship.x + Math.random()*10,
+                y: _this.ship.y + Math.random()*30 - Math.random()*20
+            };
+
+            _this.updateState(coords);
+            _this.render();
+        }, this.timeStep)
+    };
+    //run
+    this.init();
+};
+
+
